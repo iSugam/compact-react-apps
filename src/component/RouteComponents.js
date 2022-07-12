777cf1d099303a1d77b5d459dae0a0a2fff9a3de
unknown <sugamanandsahu01@gmail.com>
2022-07-12 17:23:45 +0530

    Changing some style.

--- src/component/RouteComponents.js
+++ src/component/RouteComponents.js
@@ -14,8 +14,8 @@ const RouteComponents = () => {
         <button className={!sidebarToggle? 'navButton' : 'navButton active'} onClick={toggleClick}><i className="fa-solid fa-arrow-right-long"></i></button>
         <aside className={sidebarToggle?"side-bar":"side-bar side-bar-active"}>
             <ul>
-                <li><NavLink className="nav-link" to="/"><i className="fa-solid fa-pen-clip"></i> <div>Todo List</div> </NavLink></li>
-                <li><NavLink className="nav-link" to="/resturant"> <i className="fa-solid fa-bowl-food"></i> <div>Resturant</div> </NavLink></li>
+                <li onClick={() =>setSidebarToggle(!sidebarToggle)}><NavLink className="nav-link" to="/"><i className="fa-solid fa-pen-clip"></i> <span>Todo List</span> </NavLink></li>
+                <li onClick={() =>setSidebarToggle(!sidebarToggle)}><NavLink className="nav-link" to="/resturant"> <i className="fa-solid fa-bowl-food"></i> <span>Resturant</span> </NavLink></li>
             </ul>
         </aside>
     </div>
