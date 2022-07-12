e3ff0c876f5cfdc866edd82c23f7a9c9dc0edc8b
unknown <sugamanandsahu01@gmail.com>
2022-07-12 16:35:41 +0530

    Install react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Resturant from './component/Resturant/Resturant';
+import RouteComponents from './component/RouteComponents';
 import Todo from './component/Todo/Todo';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App(props) {
   return (
     <div className="container">
-      {/* <Resturant /> */}
-      <Todo />
+      <Router>
+        <RouteComponents />
+        <Routes>
+          <Route path='/' element={<Todo />}>Todo</Route>
+          <Route path='/resturant' element={<Resturant />}>Resturant</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
