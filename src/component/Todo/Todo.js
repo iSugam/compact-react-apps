a2c00c657bf3e8632fcadebc6f8fce2c4ff09131
unknown <sugamanandsahu01@gmail.com>
2022-07-12 11:52:47 +0530

    Deleting InputTodo component for some bug and completing edit todoItems.

--- src/component/Todo/Todo.js
+++ src/component/Todo/Todo.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { InputTodo } from './InputTodo';
+// import { InputTodo } from './InputTodo';
 import "./todo.css"
 
 // Get todoItems from local storage
@@ -14,23 +14,54 @@ const getLocalData = () => {
 
 const Todo = () => {
 
-    const [todoItems, setTodoItems] = useState(getLocalData())
+    const [inputData, setInputData] = useState("");
+    const [todoItems, setTodoItems] = useState(getLocalData());
+    const [editItem, setEditItem] = useState("");
+    const [toggleBtn, setToggleBtn] = useState(false);
 
-    // Adding todo items to list
-    const addItems = (input, setInput) => {
-        const inputData = {
-            id: `id-is-${todoItems.length}`,
-            value: input
-        }
-        if(!input) return
-        setTodoItems(prevValue => [...prevValue, inputData])
-        setInput("")
+    // Handle input value
+    const handleChange = e => {
+        const value = e.target.value;
+        setInputData(value)
     }
 
-    // Adding todoItems to local storage 
+    // Adding todo items to list
+    const addItems = () => {
+        const newInputData = {
+            id: `id-is-${todoItems.length}`,
+            value: inputData
+        }
+
+        if(!inputData) return
+        else if(inputData && toggleBtn) {
+            setTodoItems(todoItems.map(curItem => {
+                if(curItem.id === editItem) {
+                    return {...curItem, value: inputData}
+                } else return curItem
+            }))
+            setInputData("");
+            setEditItem(null);
+            setToggleBtn(false);
+        }
+        
+        else {setTodoItems(prevValue => [...prevValue, newInputData])
+        setInputData("")}
+    } 
+
+    // Adding todoItems to local storage with useEffect
     useEffect(() => {
         localStorage.setItem("todoList", JSON.stringify(todoItems));
     }, [todoItems])
+
+    // Function for editing item
+    const onEditItems = (id) => {
+        const toEdit = todoItems.find(item => {
+            return item.id === id;
+        })
+        setInputData(toEdit.value);
+        setEditItem(id);
+        setToggleBtn(true);
+    }
 
     // Deleting todoItems from list
     const deleteItems = (id) => {
@@ -55,7 +86,19 @@ const Todo = () => {
                 </figcaption>
             </figure>
 
-            <InputTodo onAddItems = {addItems}/>
+            <div className="input-container">
+                <input 
+                    type="text" 
+                    onChange={handleChange}
+                    placeholder='📒 Write your Todo'
+                    value={inputData}
+                />
+
+                {toggleBtn ? 
+                    <span onClick={addItems}><i className="fa-solid fa-pen-to-square"></i></span>
+
+                    : <span onClick={addItems}><i className="fa-solid fa-plus"></i></span>}
+            </div>
 
             <div className="added-list">
                 <ul>
@@ -63,7 +106,7 @@ const Todo = () => {
                         return (
                             <li key={item.id}> {item.value}
                                 <div className='list-icon'>
-                                    <span><i className="fa-solid fa-pen-to-square"></i></span>
+                                    <span onClick={() =>onEditItems(item.id)}><i className="fa-solid fa-pen-to-square"></i></span>
                                     <span onClick={() =>deleteItems(item.id)}><i className="fa-solid fa-trash-can"></i></span>
                                 </div>
                             </li>
